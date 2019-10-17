6facd5daab0ab993c3c9f84967e27fe73979e029
Leonardo Okuma <okuma@textecnologia.com.br>
2019-10-17 15:38:38 -0300

    Prosseguindo desenvolvimento da view do WhatsApp e dos querySelectors assíncronos

--- views/ChatView.js
+++ views/ChatView.js
@@ -9,11 +9,12 @@
 		this._newMessages = [];
 	}
 
-	querySelector(selector, rootElement = document) {
+	querySelector(selector, fulfillmentCallback = (element, timeElapsed) => (element || timeElapsed > 3000), rootElement = document) {
 		return new Promise((resolve, reject) => {
-			let intervalID = setInterval(() => {
-				let element = rootElement.querySelector(selector);
-				if (element) {
+			let timeElapsed = 0;
+			const intervalID = setInterval(() => {
+				const element = rootElement.querySelector(selector);
+				if (fulfillmentCallback(element, timeElapsed += 100)) {
 					clearInterval(intervalID);
 					resolve(element);
 				}
@@ -22,11 +23,12 @@
 		});
 	}
 
-	querySelectorAll(selector, rootElement = document) {
+	querySelectorAll(selector, fulfillmentCallback = (elements, timeElapsed) => (elements.length || timeElapsed > 3000), rootElement = document) {
 		return new Promise((resolve, reject) => {
-			let intervalID = setInterval(() => {
-				let elements = rootElement.querySelectorAll(selector);
-				if (elements.length > 1) {
+			let timeElapsed = 0;
+			const intervalID = setInterval(() => {
+				const elements = rootElement.querySelectorAll(selector);
+				if (fulfillmentCallback(elements, timeElapsed += 100)) {
 					clearInterval(intervalID);
 					resolve(elements);
 				}
@@ -39,46 +41,36 @@
 		return this._title;
 	}
 
-	set title(value) {
-		this._title = value;
-	}
-
 	checked() {
 		this._checked = true;
 		this._newMessages.length = 0;
 	}
 
 	hasNewMessage() {
- 		// #TODO true somente se a conversa tiver bolinha verde
-		return !!document.querySelector('.P6z4j._1W1Se[title="' + this._title + '"]');
-
-		document.querySelector('._2UaNq._2ko65').dispatchEvent(new MouseEvent('mousedown', {bubbles: true, cancelable: true, view: window})); // #TODO retorna as conversas (para clicar em cima) que tiverem bolinha verde de alerta
-		document.querySelectorAll('.P6z4j._1W1Se'); // #TODO retorna as conversas que tiverem bolinha verde de alerta
-		this._hasNewMessage = true;
-		
-		// #TODO arrumar
-		return true;
+		// #TODO funcionando para bolinha verde. Ainda é incerto se funciona quando é recebida uma mensagem na conversa atual
+		return this._hasNewMessage = !!document.querySelector('span._19RFN[title="+55 11 99814-9449"]')
+			.parentNode.parentNode.parentNode.querySelector('span.P6z4j._1W1Se');
 	}
 
 	getNewMessages() {
 		return new Promise((resolve, reject) => {
 			document.querySelector('span._19RFN[title="' + this._title + '"]').dispatchEvent(new MouseEvent('mousedown', {bubbles: true, cancelable: true, view: window}));
 			
-			// #TODO retorna todas abaixo do Unread Messages
-			this._newMessages = this.querySelectorAll(/*'._3Xx0y ~*/ 'div.message-in span.selectable-text span').then(elements => resolve([...elements].reverse()));
+			// #TODO retorna todas as mensagens abaixo do "Unread Messages"
+			this._newMessages = this.querySelectorAll('._3Xx0y ~ div.message-in span.selectable-text span').then(elements => resolve([...elements].reverse()));
 		});
 	}
 
-	postMessage(msg) {
-		// #TODO mover para a conversa certa e depois rodar o código abaixo
-		document.querySelector('._3u328').textContent = msg;
-		document.querySelector('._3u328').dispatchEvent(new Event('input', {bubbles: true}));
-		document.querySelector('._3M-N-').click();
+	async postMessage(msg) {
+		document.querySelector('span._19RFN[title="' + this._title + '"]').dispatchEvent(new MouseEvent('mousedown', {bubbles: true, cancelable: true, view: window}));
+		let chat = await this.querySelector('._3u328');
+		chat.textContent = msg;
+		chat.dispatchEvent(new Event('input', {bubbles: true, cancelable: true, view: window}));
+		document.querySelector('._3M-N-').click(); // Checar se não haverá problema de sincronia
 	}
 
-	getMessages() {
-		// #TODO mensagem mais recente na posição 0
-		this._hasNewMessage = false;
-		return [...document.querySelectorAll('div.message-in span.selectable-text span')].reverse();
+	async getMessages() {
+		document.querySelector('span._19RFN[title="' + this._title + '"]').dispatchEvent(new MouseEvent('mousedown', {bubbles: true, cancelable: true, view: window}));
+		return [...await this.querySelectorAll('div.message-in span.selectable-text span')].reverse();
 	}
 }
