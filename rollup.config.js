13e9a0ad949faadb27fb7a5e22c7def85aa0b08c
Leokuma <leonardo.okuma@gmail.com>
2020-03-29 14:19:27 -0300

    #10 Build options
    'PRODUCTION' renamed to 'RELEASE', and 'DEVELOPMENT' is now default (no arguments).
    
    Only 'RELEASE' generates the minified source now.

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,30 @@
 import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
 
+let output = [{
+	file: 'jsquadbot.mjs',
+	format: 'esm'
+}];
+
+if (process.env.RELEASE) {
+	output.push({
+		file: 'jsquadbot.min.mjs',
+		format: 'esm',
+		plugins: [
+			terser({
+				keep_classnames: true,
+				module: true,
+				compress: {defaults: false, drop_console: true}
+			})
+		]
+	});
+}
+
 export default {
 	input: 'loader.mjs',
 	plugins: [
 		eslint()
 	],
 	treeshake: false,
-	output: [
-		{
-			file: 'jsquadbot.mjs',
-			format: 'esm'
-		},
-		{
-			file: 'jsquadbot.min.mjs',
-			format: 'esm',
-			plugins: [
-				terser({
-					keep_classnames: true,
-					module: true,
-					compress: {defaults: false, drop_console: true}
-				})
-			]
-		}
-	]
+	output: output
 };
